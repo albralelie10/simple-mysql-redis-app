43c5dc30d5234f16ec29447154cefd2ce5219f47
polo <polo@contoso.com>
2023-09-05 21:24:46 -0600

    proyect full redis-mysqlc is working

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import axios from "axios";
 import {createClient} from "redis"
 import cors from "cors"
+import mysql from "mysql2"
+
 
 const app = express();
 // Create Redis Client
@@ -9,6 +11,22 @@ app.use(express.json())
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
 
+
+//mysql Client
+const db=mysql.createConnection({
+  host:"127.0.0.1",
+  user:"root",
+  password:"Albralelie12@",
+  database:"sluts",
+})
+
+db.connect((err)=>{
+  if(err){
+    console.log("Connection with MYSQL Failed",err)
+  }else{
+    console.log("Connect with MYSQL DB.....")
+  }
+})
 
 const client=createClient();
 client.on("error",(error)=>console.log("Redis client Error",error))
@@ -33,7 +51,7 @@ const DEFAULT_EXPIRATION=3600
 app.get("/photos",async(req,res)=>{
   try{
   const albumId=req.query.albumId;
-  const dataRequest=await getCacheStatus(`photos:${albumId}`,async ()=>{
+  const dataRequest=await getCacheStatus(`photos?albumId:${albumId}`,async ()=>{
     const {data}=await axios.get("https://jsonplaceholder.typicode.com/photos",{
         params:{
           albumId
@@ -47,6 +65,17 @@ app.get("/photos",async(req,res)=>{
   }
 })
 
+app.get("/photos/:id",async(req,res)=>{
+  try{
+  const dataRequest=await getCacheStatus(`photos:${req.params.id}`,async ()=>{
+    const {data}=await axios.get(`https://jsonplaceholder.typicode.com/photos/${req.params.id}`)
+      return data;
+  })
+    return res.json(dataRequest)
+  }catch(err){
+    return res.status(500).json(err)
+  }
+})
 
 function  getCacheStatus(key,cb){
   return new Promise(async(resolve,reject)=>{
@@ -63,26 +92,27 @@ function  getCacheStatus(key,cb){
   })
 }
 
+app.get('/database', (req, res) => {
+
+  const q="SELECT * FROM my_sluts";
+  db.query(q,async(err,data)=>{
+    if(err)return res.status(500).json(err)
+    const value=await client.get("nombre")
+    if(value){
+      console.log("Hit value from redis")
+      return res.json(JSON.parse(value))
+    }else{
+      console.log("Cache MISS")
+      client.setEx("nombre",DEFAULT_EXPIRATION,JSON.stringify(data))
+      return res.json(data)
+    }
+  })
+  });
+
 app.listen(3000, () => console.log("Listening on port 3000"));
 
 
-// app.get('/database', (req, res) => {
 
-//   const q="SELECT * FROM my_sluts";
-//   db.query(q,async(err,data)=>{
-//     if(err)return res.status(500).json(err)
-//     const value=await client.get("nombre")
-//     if(value){
-//       console.log("Hit value from redis")
-//       return res.json(JSON.parse(value))
-//     }else{
-//       console.log("Cache MISS")
-//       client.setEx("nombre",DEFAULT_EXPIRATION,JSON.stringify(data))
-//       return res.json(data)
-//     }
-//   })
-//   });
 
-// app.listen(3000, () => console.log("Listening on port 3000"));
 
 
